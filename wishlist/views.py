60f759bac1abbc6226ae328cbc8eb85774eeccc9
joselinprmt <joselin.permata@ui.ac.id>
2022-09-15 03:25:15 +0700

    LAB 2: Implementasi data delivery dengan xml dan json

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from wishlist.models import BarangWishlist
 
 
@@ -9,3 +11,18 @@ def show_wishlist(request):
         'nama': 'Joselin Permata Aprillia'
     }
     return render(request, "wishlist.html", context)
+
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
